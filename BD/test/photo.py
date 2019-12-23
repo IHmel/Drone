259f5356cb00184d2d38d6235551ea76b9896f3d
ilya132075 <47531259+ilya132075@users.noreply.github.com>
2019-12-23 18:05:08 +0300

    Delete photo.py

--- BD/test/photo.py
+++
@@ -1,6 +0,0 @@
-import image  
-# open method used to open different extension image file 
-im = Image.open(r"C:\Users\Lucifer\Pictures\8yClrQ14NcM.jpg")  
-  
-# This method will show image in any image viewer  
-im.show()  
